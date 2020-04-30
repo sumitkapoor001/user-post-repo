18bd2348ee05115f616d54d7d4a5ef016a0e0b34
Sumit Kapoor <30923069+sumitkapoor001@users.noreply.github.com>
2020-04-30 13:09:02 -0400

    Update UserPostApplication.java

--- UserPost/src/main/java/com/spf/UserPostApplication.java
+++ UserPost/src/main/java/com/spf/UserPostApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserPostApplication {
-
+	//Starting
 	public static void main(String[] args) {
 		SpringApplication.run(UserPostApplication.class, args);
 	}
